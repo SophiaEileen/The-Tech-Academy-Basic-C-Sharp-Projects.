0b8ad67016bfa409f525a66af4b7306bc9fbf280
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-08-25 14:46:20 -0400

    Add files via upload

---
+++ MathOperations/MathOperations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 5;
+            //int num2 = 7;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //int num1 = 5;
+            //int num2 = 7;
+            //int difference = num1 - num2;
+            //Console.WriteLine(difference);
+            //Console.ReadLine();
+
+            //int num1 = 5;
+            //int num2 = 7;
+            //int product = num1 * num2;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int num1 = 12;
+            //int num2 = 3;
+            //int quotient = num1 / num2;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            // this will return the remainder when two numbers are divided. % is "modulus operator". 
+            //int num1 = 7;
+            //int num2 = 3;
+            //int remainder = num1 % num2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            int number = 7;
+            int remainder = number % 2;
+            Console.WriteLine(remainder);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
