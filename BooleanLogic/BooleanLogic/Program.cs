6f53ec190e73e1c8cdd15b9f171ae13efd950009
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-08-26 23:07:27 -0400

    Add files via upload

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BOOLEAN LOGIC (&&)
+            //int packageWidth = 10;
+            //int packageHeight = 25;
+            //bool tooBig = (packageHeight > 25 && packageWidth > 25);
+            //Console.WriteLine(tooBig);
+            //Console.ReadLine();
+
+            //BOOLEAN LOGIC (||)
+            //int packageWidth = 10;
+            //int packageHeight = 25;
+            //bool tooBig = (packageHeight > 25 || packageWidth > 25);
+            //Console.WriteLine(tooBig);
+            //Console.ReadLine();
+
+            //BOOLEAN LOGIC MULTIPLE USES OF AND
+            //string firstName = "Jesse";
+            //string lastName = "Johnson";
+            //string socialSecurityNumber = "111-11-1111";
+
+            //bool isAuthorized = (firstName == "Jesse" && lastName == "Johnson" && socialSecurityNumber == "111-11-1111");
+            //Console.WriteLine(isAuthorized);
+            //Console.ReadLine();
+
+            //BOOLEAN LOGIC MULTIPLE USES OF OR
+            //int bankAccountBalance = 500;
+            //bool isInARockBand = false;
+            //string name = "Jesse";
+
+            //bool isReallyCool = (bankAccountBalance > 100000 || isInARockBand || name == "Jesse");
+
+            //Console.WriteLine(isReallyCool);
+            //Console.ReadLine();
+
+            //BOOLEAN LOGIC COMBINING && AND ||
+            //bool result = (true && (true || false));
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            int num1 = 7;
+            int num2 = 12;
+            int num3 = 23;
+            int num4 = 14;
+            int num5 = 8;
+            int num6 = 5;
+
+            bool result = (num1 > num2 && (num3 == num4 || num5 > num6));
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
