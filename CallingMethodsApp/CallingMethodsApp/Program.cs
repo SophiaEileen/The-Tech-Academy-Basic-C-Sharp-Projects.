10cc6a56611330787aa66060a9e4198fa6ca2c68
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-02 17:33:54 -0400

    Add files via upload

---
+++ CallingMethodsApp/CallingMethodsApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input a second integer (optional): ");
+            bool userNum2IsValid = int.TryParse(Console.ReadLine(), out int userNum2);
+
+            myMath myNumObj = new myMath();
+
+            int result = 0;
+            if (userNum2IsValid)
+            {
+                result = myNumObj.AddInts(userNum, userNum2);
+            }
+            else
+            {
+                result = myNumObj.AddInts(userNum);
+            }
+
+
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
