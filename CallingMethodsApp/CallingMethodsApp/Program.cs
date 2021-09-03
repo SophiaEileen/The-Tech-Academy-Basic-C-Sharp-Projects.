a9db7338585d9f30160cf35d84a5f12a405a8db5
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-02 23:39:59 -0400

    Add files via upload

---
+++ CallingMethodsApp/CallingMethodsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate Methods class
+            Methods newMethod = new Methods();
+
+            //User number is divided by two and is output within the method
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            newMethod.MethodsOp(userNum);
+
+            //static overload method with an out parameter
+            int myNum = 1;
+            Console.WriteLine("Your number plus my number is: " + Methods.MethodsOp(userNum, myNum));
+            newMethod.MethodsOut(userNum, out int extraNum);
+
+            Console.WriteLine("That number plus one is: " + extraNum);
+            Console.ReadLine();
+        }
+       
+    }
+}
