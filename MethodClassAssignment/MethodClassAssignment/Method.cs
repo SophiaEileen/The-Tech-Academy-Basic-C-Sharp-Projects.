47ce1e70487224dc2928d4887468e949b4747f36
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-03 01:05:21 -0400

    Add files via upload

---
+++ MethodClassAssignment/MethodClassAssignment/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Method
+    {
+        public void myOperation(int firstNum, int secondNum)
+        {
+            int myNumAdd = firstNum + 10;
+            Console.WriteLine("The second number is: " + secondNum);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 10;
+            int myNum2 = 12;
+
+            //instantiate the class
+            Method myOp = new Method();
+
+            //Call the method in the class, passing in two numbers.
+            myOp.myOperation(myNum, myNum2);
+
+            //Call the method in the class, specifying the parameters by name.
+            myOp.myOperation(secondNum: myNum2, firstNum: myNum);
+        }
+    }
+    }
+}
