8b3c95fdd2858e7f4a6745710f7e722404a2b49d
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-04 15:53:58 -0400

    Add files via upload

---
+++ MethodParametersAssignment/MethodParametersAssignment/MathOp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodParametersAssignment
+{
+    class MathOp
+    {
+        //math method operation and return an integer result.
+        public int AddInts(int userNum, int userNum2 = 0)
+        {
+            int newUserNum = userNum + userNum2;
+            return newUserNum;
+        }
+    }
+}
---
+++ MethodParametersAssignment/MethodParametersAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //takes int as parameter
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            //takes int as parameter (optional)
+            Console.WriteLine("Please input a second integer (optional): ");
+            bool userNum2IsValid = int.TryParse(Console.ReadLine(), out int userNum2);
+            
+            //instantiate class
+            MathOp myNumObj = new MathOp();
+
+            //if two numbers - will add them otherwise it will return input int. 
+            int result = 0;
+            if (userNum2IsValid)
+            {
+                result = myNumObj.AddInts(userNum, userNum2);
+            }
+            else
+            {
+                result = myNumObj.AddInts(userNum);
+            }
+
+
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
