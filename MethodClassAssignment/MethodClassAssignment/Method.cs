973b68d28f4e4a6938ecad209ba67bb13d422733
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-04 17:28:26 -0400

    Add files via upload

---
+++ MethodClassAssignment/MethodClassAssignment/Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Method
+    {
+        public void mathOp(int firstNum, int secondNum)
+        {
+            firstNum += 10;
+            Console.WriteLine("The second input is: " + secondNum);
+            
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            //instantiate the class
+            Method myOp = new Method();
+
+            //Call the method in the class, passing in two numbers.
+            myOp.mathOp(num1, num2);
+
+            //Call the method in the class, specifying the parameters by name.
+            myOp.mathOp(secondNum: num2, firstNum: num1);
+            Console.ReadLine();
+        }
+    }
+    
+}
