ca49f214417f1c51e0aa0b276e3d066948fe02f3
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-04 15:09:11 -0400

    Add files via upload

---
+++ CallingMethodsApp/CallingMethodsApp/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsApp
+{
+    public class Methods
+    {
+        public static int addInt(int num)
+        {
+            int myNum = num;
+            int myNumAdd = myNum + 10;
+            return myNumAdd;
+        }
+
+        public static int subInt(int num)
+        {
+            int myNum = num;
+            int myNumSub = myNum - 5;
+            return myNumSub;
+        }
+
+        public static int multInt(int num)
+        {
+            int myNum = num;
+            int myNumMult = myNum * 10;
+            return myNumMult;
+        }
+    }
+}
---
+++ CallingMethodsApp/CallingMethodsApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer to perform basic math operations on.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            int newNumAdd = Methods.addInt(userNum);
+            int newNumSub = Methods.subInt(userNum);
+            int newNumMult = Methods.multInt(userNum);
+
+            Console.WriteLine("Your number plus 10 is: " + newNumAdd);
+            Console.WriteLine("Your number subtracted by 5 is: " + newNumSub);
+            Console.WriteLine("Your number multiplied by 10 is: " + newNumMult);
+            Console.ReadLine(); 
+        }
+        
+       
+    }
+}
