18646694e85262fb806a5bf6f28948f12eb94a09
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-05 01:06:00 -0400

    Add files via upload

---
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Employee : Person
+    {
+        // superclass property defintion 
+        int id { get; set; }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+   public class Person
+    {
+        //person name definition 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //method to display persons first and last name 
+        public void sayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+        
+    }
+}
