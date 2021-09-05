d334defedff6d0b9294ab782c615e209d1f50a43
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-04 20:24:59 -0400

    Add files via upload

---
+++ Class.OverloadMethodAssignment/Class.OverloadMethodAssignment/StaticMathOp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.OverloadMethodAssignment
+{
+    public static class StaticMathOp
+    {
+        //using out parameter 
+        public static int MethodMult(int userNum)
+        {
+            int returnNum = userNum * 5;
+            return returnNum;
+        }
+    }
+}
