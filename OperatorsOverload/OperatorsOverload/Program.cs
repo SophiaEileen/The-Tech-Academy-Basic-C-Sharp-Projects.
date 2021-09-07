baf489399a4998cc7c2c6717ec782c74c96fc2e3
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-07 13:22:44 -0400

    Add files via upload
    Operators Submission

---
+++ OperatorsOverload/OperatorsOverload/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorsOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee1 = new Employee();
+            //employee1.firstName = "Sample";
+            //employee1.lastName = "Student";
+            //employee1.SayName();
+            //Console.ReadLine();
+            //employee1.Quit();
+            //Console.WriteLine(employee1.firstName + "... " + employee1.lastName);
+            //Console.ReadLine();
+
+            Employee employee2 = new Employee();
+            employee2.firstName = "Sophia";
+            employee2.lastName = "Eileen";
+            employee2.employeeID = 8956;
+
+            Employee employee3 = new Employee();
+            employee3.firstName = "Patrick";
+            employee3.lastName = "Howle";
+            employee3.employeeID = 8956;
+
+            bool doesEqual = employee2 == employee3; //overloaded operater method. 
+
+            Console.ReadLine();
+        }
+    }
+}
