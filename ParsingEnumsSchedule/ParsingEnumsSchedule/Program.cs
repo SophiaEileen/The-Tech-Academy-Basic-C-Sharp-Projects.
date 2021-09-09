d3d0166542f05d2ff8a04e07c3e262ddf0f16b2c
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-09 15:53:34 -0400

    Add files via upload

---
+++ ParsingEnumsSchedule/ParsingEnumsSchedule/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsSchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the day of the week: ");
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine(), true);
+                Console.WriteLine("The date is: {0}", day.ToString());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
