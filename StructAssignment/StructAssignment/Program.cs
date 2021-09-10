eb329bac57ef644d7fdff37aeae7b0a9861625b1
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-10 01:26:55 -0400

    Add files via upload

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number myNum = new Number();
+            myNum.Amount = 7.7m;
+            Console.WriteLine(myNum.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
