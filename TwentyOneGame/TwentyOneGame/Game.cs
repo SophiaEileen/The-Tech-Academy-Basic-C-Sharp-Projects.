308ceb79c88b7c8c5fb68ba68b861cb14f5721c4
Sophia <87103287+SophiaEileen@users.noreply.github.com>
2021-09-10 20:16:55 -0400

    Add files via upload

---
+++ TwentyOneGame/TwentyOneGame/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public abstract class Game //abstract class does not have implementation, can never be instantiated. 
+    {
+        public List<Player> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+
+        public abstract void Play(); //abstract method
+
+        public virtual void ListPlayers() //virtual method inside of abstract class. Have implemenation but can be overrided 
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+        }
+    }
+}
